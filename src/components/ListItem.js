55f1ba548129d7a3011204e4af79d353b6549c9e
Mahdi <mahdi@LearniOSFreecoms-Mac.local>
2017-01-01 18:39:22 -0800

    Added action creator

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,21 @@
 import React, {Component} from "React";
 import {Text} from "react-native";
+import {connect} from "react-redux";
 import {CardSection} from "./common";
+import * as actions from "../actions";
 
 class ListItem extends Component {
 
 	render() {
 
 		const {titleStyle} = styles;
+		console.log(this.props);
 
 		return (
 			<CardSection>
-				<Text style={titleStyle}>{this.props.library.title}</Text>
+				<Text style={titleStyle}>
+					{this.props.library.title}
+				</Text>
 			</CardSection>
 		);
 	}
@@ -24,4 +29,4 @@ const styles = {
 	}
 };
 
-export default ListItem;
+export default connect(null,actions)(ListItem);
